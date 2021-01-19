926f3b73fc30acf7756060fdb16e083935b43bbc
root <tando1903@gmail.com>
2021-01-19 10:30:46 +0700

    still first but 5 minutes later

---
+++ extras/cppapi/build/CMakeFiles/retinanet.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CUDA"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CUDA
+  "/workspace/retinanet-examples/csrc/cuda/decode.cu" "/workspace/retinanet-examples/extras/cppapi/build/CMakeFiles/retinanet.dir/workspace/retinanet-examples/csrc/cuda/decode.cu.o"
+  "/workspace/retinanet-examples/csrc/cuda/decode_rotate.cu" "/workspace/retinanet-examples/extras/cppapi/build/CMakeFiles/retinanet.dir/workspace/retinanet-examples/csrc/cuda/decode_rotate.cu.o"
+  "/workspace/retinanet-examples/csrc/cuda/nms.cu" "/workspace/retinanet-examples/extras/cppapi/build/CMakeFiles/retinanet.dir/workspace/retinanet-examples/csrc/cuda/nms.cu.o"
+  "/workspace/retinanet-examples/csrc/cuda/nms_iou.cu" "/workspace/retinanet-examples/extras/cppapi/build/CMakeFiles/retinanet.dir/workspace/retinanet-examples/csrc/cuda/nms_iou.cu.o"
+  )
+set(CMAKE_CUDA_COMPILER_ID "NVIDIA")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CUDA
+  "retinanet_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CUDA_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/include/opencv"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/workspace/retinanet-examples/csrc/engine.cpp" "/workspace/retinanet-examples/extras/cppapi/build/CMakeFiles/retinanet.dir/workspace/retinanet-examples/csrc/engine.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "retinanet_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/cuda/include"
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ extras/cppapi/build/CMakeFiles/retinanet.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/retinanet.dir/workspace/retinanet-examples/csrc/cuda/decode.cu.o"
+  "CMakeFiles/retinanet.dir/workspace/retinanet-examples/csrc/cuda/nms.cu.o"
+  "CMakeFiles/retinanet.dir/workspace/retinanet-examples/csrc/cuda/decode_rotate.cu.o"
+  "CMakeFiles/retinanet.dir/workspace/retinanet-examples/csrc/cuda/nms_iou.cu.o"
+  "CMakeFiles/retinanet.dir/workspace/retinanet-examples/csrc/engine.cpp.o"
+  "CMakeFiles/retinanet.dir/cmake_device_link.o"
+  "libretinanet.pdb"
+  "libretinanet.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/retinanet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
